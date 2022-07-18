67705fd0de247923f99a2cbc05b7fcc7457b03fb
saitozdemir <saitozdemir@icloud.com>
2022-07-19 02:26:24 +0300

    edit

---
+++ DataAccess/Concrete/EfBaseRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace DataAccess.Concrete
+{
+    public class EfBaseRepository<TEntity, TDBContext> : IBaseRepository<TEntity> where TEntity : class, IEntity, new()
+    where TDbContext : DbContext, new()
+    {
+        public Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+                return filter == null
+                    ? await context.Set<TEntity>().ToListAsync()
+                    : await context.Set<TEntity>().Where(filter).ToListAsync();
+                
+            }
+}
+
+public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+{
+    return await context.Set<TEntity>().SingleOrDefaultAsync(filter);
+}
+
+public Task<TEntity> AddAsync(TEntity entity)
+{
+    throw new NotImplementedException();
+}
+
+public Task<TEntity> UpdateAsync(TEntity entity)
+{
+    throw new NotImplementedException();
+}
+
+public Task<bool> DeleteAsync(int id)
+{
+    throw new NotImplementedException();
+}
+    }
+}
--- DataAccess/IBaseRepository.cs
+++ DataAccess/IBaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.Abstract;
@@ -9,5 +10,10 @@ namespace DataAccess
 {
     public interface IBaseRepository<T> where T : class, IEntity, new()
     {
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
     }
 }
