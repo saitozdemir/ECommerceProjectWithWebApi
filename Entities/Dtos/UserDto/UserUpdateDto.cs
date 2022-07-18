d694d93a22787576d146ad786f582101b4f1b1ce
saitozdemir <saitozdemir@icloud.com>
2022-07-18 22:52:59 +0300

    edit

---
+++ Entities/Dtos/UserDto/UserUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Dtos.UserDto
+{
+    public class UserUpdateDto:IDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
